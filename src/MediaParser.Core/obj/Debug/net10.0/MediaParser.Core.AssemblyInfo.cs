e58453fe69533523d07f552074c2c3471861fe0a
HPEL <3527206832@qq.com>
2026-02-04 20:58:49 +0800

    0.2.1

--- src/MediaParser.Core/obj/Debug/net10.0/MediaParser.Core.AssemblyInfo.cs
+++ src/MediaParser.Core/obj/Debug/net10.0/MediaParser.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaParser.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e5c4d5e05f6fd8e5079472f79cb0f9cc2d28fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4710ff6c154fad14fa7fba606410a9b56b2108")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaParser.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaParser.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
